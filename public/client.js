911e3c2ed57ad003fa22ee630c6fef684ace34fc
Thilo Roth <thilo@thilo.io>
2016-11-07 15:42:25 +0100

    basic angular strcture fp

---
+++ public/client.js
@@ -0,0 +1,14 @@
+(function(){
+    const linkbinApp = angular.module('linkbinApp', []);
+
+    linkbinApp.controller('frontPageList', ($scope, $http) => {
+        $scope.load = () => {
+            $http.get('/homepage')
+            .then(content => {
+                $scope.links = content.data.rows;
+            });
+        };
+        $scope.load();
+    });
+
+})();
