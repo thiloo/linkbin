bf120e86644fbca5bed555de77453fa3726acc81
Thilo Roth <thilo@thilo.io>
2016-11-07 21:40:04 +0100

    securing

--- public/client.js
+++ public/client.js
@@ -1,17 +1,31 @@
 (function(){
     const linkbinApp = angular.module('linkbinApp', []);
 
-    linkbinApp.controller('frontPageList', ($scope, $http) => {
+    linkbinApp.controller('header', ($scope) => {
+
+    });
+
+    linkbinApp.controller('frontPageListView', ($scope, $http) => {
         $scope.load = () => {
             $http.get('/homepage')
             .then(content => {
-                console.log(content);
                 $scope.links = content.data.file;
-                console.log($scope);
             })
             .catch(error => console.log(error));
         };
         $scope.load();
     });
+
+    linkbinApp.controller('singleLinkView', ($scope, $http, $location) => {
+        $scope.load = () => {
+            $http.get(`/${$location.path}`)
+            .then(content => {
+                $scope.singleView = content.data.file;
+            })
+            .catch(error => console.log(error));
+        };
+    });
+
+
 
 })();
