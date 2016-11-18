9ba1f9f8e20eae7b6e6dcde0a93b5aa19b7b7993
Thilo Roth <thilo@thilo.io>
2016-11-18 12:55:14 +0100

    testing bug

--- modules/duplicate_check.js
+++ modules/duplicate_check.js
@@ -5,14 +5,16 @@ module.exports.check = function(link) {
     return new Promise(function(resolve, reject) {
         // var parsed = url.parse(link),
         //     cleanUrl= `${parsed.protocol}//${parsed.hostname}${parsed.pathname}`;
-        var cleanUrl = clean(link);
-        db.checkForDuplicateUrl(cleanUrl).then(function(result) {
-            if(result.rows.length > 0) {
-                reject('This link was already shared on linkbin! Please only post new content');
-            } else {
-                resolve(cleanUrl);
-            }
+        clean(link, function(err, cleanUrl) {
+            db.checkForDuplicateUrl(cleanUrl).then(function(result) {
+                if(result.rows.length > 0) {
+                    reject('This link was already shared on linkbin! Please only post new content');
+                } else {
+                    resolve(cleanUrl);
+                }
+            });
         });
+
     });
 };
 
